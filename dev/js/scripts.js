5857cb2cb149223a22056ac3ce6ac5055a2c76f3
lca66 <102632441+lca66@users.noreply.github.com>
2023-05-30 18:56:24 -0400

    FINAL_SCROLLTRIGGER

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,8 +16,8 @@ gsap.from("#section-1 h1",{duration:1, scrollTrigger:{
     trigger:"#section-1",
     toggleActions:"play pause resume reverse",
     start:"top 0%",
-    end:"bottom 25%",
-    markers:true
+    end:"bottom 25%"
+    // markers:false,
     // (property) startColor: white,
     // (property) endColor: string
     // markers: {startColor: "--black", endColor: "--black"}
@@ -31,7 +31,7 @@ gsap.from("#section-1 h1",{duration:1, scrollTrigger:{
 //     toggleActions:"play pause resume reverse",
 //     start:"top 80%",
 //     end:"bottom 20%",
-//     markers:true
+
 //     },
 //     scale:.5,});
 
@@ -41,8 +41,8 @@ gsap.from("#section-1 h1",{duration:1, scrollTrigger:{
     trigger:"#section-1",
     toggleActions:"play pause resume reverse",
     start:"top 0%",
-    end:"bottom 25%",
-    markers:true
+    end:"bottom 25%"
+    markers:false,
     },
     scale:.75,   
     y:"-=700%"});   
@@ -54,8 +54,8 @@ gsap.from("#box",{duration:2, scrollTrigger:{
     trigger:"#box",
     toggleActions:"play pause resume reverse",
     start:"top 60%",
-    end:"bottom 40%",
-    markers:true
+    end:"bottom 40%"
+    markers:false,
     },
     scale:.7, 
     x:"=100%"});
@@ -64,8 +64,8 @@ gsap.from("#section-2",{duration:1, scrollTrigger:{
     trigger:"#section-2",
     toggleActions:"play pause resume reverse",
     start:"top 40%",
-    end:"bottom 60%",
-    markers:true
+    end:"bottom 60%"
+    markers:false,
     },
     scale:.95, 
     x:"=100%"});
@@ -74,8 +74,8 @@ gsap.from("#box2",{duration:2, scrollTrigger:{
     trigger:"#box2",
     toggleActions:"play pause resume reverse",
     start:"bottom 70%",
-    end:"top 30%",
-    markers:true
+    end:"top 30%"
+    markers:false,
     },
     scale:.7, 
     x:"=100%"});
@@ -84,8 +84,8 @@ gsap.from("#section-3",{duration:1, scrollTrigger:{
     trigger:"#section-3",
     toggleActions:"play pause resume reverse",
     start:"top 40%",
-    end:"bottom 60%",
-    markers:true
+    end:"bottom 60%"
+    markers:false,
     },
     scale:.95, 
     x:"=100%"});
@@ -94,8 +94,8 @@ gsap.from("#box3",{duration:2, scrollTrigger:{
     trigger:"#box3",
     toggleActions:"play pause resume reverse",
     start:"bottom 80%",
-    end:"top 20%",
-    markers:true
+    end:"top 20%"
+    markers:false,
     },
     scale:.7, 
     x:"=100%"});
@@ -104,8 +104,8 @@ gsap.from("#section-4",{duration:1, scrollTrigger:{
     trigger:"#section-4",
     toggleActions:"play pause resume reverse",
     start:"top 60%",
-    end:"bottom 40%",
-    markers:true
+    end:"bottom 40%"
+    markers:false
     },
     scale:.95, 
     x:"=100%"});
